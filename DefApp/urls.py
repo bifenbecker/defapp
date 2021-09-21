cffec7a232cbdfbfeab88c9e46f7b0e54e540657
bifenbecker <b1fenbecker@mail.ru>
2021-09-21 18:37:04 +0300

    Initial commit Add app accounts: login, logout, registration, profile add Postgres DB

---
+++ DefApp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('main/', MainPageReDirect.as_view(), name='main_page_url'),
+]
---
+++ DefApp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.views import View
+
+class MainPageReDirect(View):
+    template_name = 'main.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
