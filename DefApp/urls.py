6a338a63c3eb0ae9e84a1e2ffaeb9b2e0f32defa
bifenbecker <b1fenbecker@mail.ru>
2021-09-23 15:48:06 +0300

    Test

--- DefApp/urls.py
+++ DefApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('main/', MainPageReDirect.as_view(), name='main_page_url'),
+    path('posts/', include('posts.urls')),
 ]
